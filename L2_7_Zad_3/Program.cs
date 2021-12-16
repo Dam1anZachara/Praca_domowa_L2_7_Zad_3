005ff74ac8b03c3572cdebbd0f1d5ad8dad90912
dzachara <dzachara@ZACHARA-D>
2021-12-16 13:31:26 +0100

    Add project files.

---
+++ L2_7_Zad_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace L2_7_Zad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The program will check if the number is positive or negative ");
+            Console.WriteLine("Please enter an intiger number.");
+
+            Int32.TryParse(Console.ReadLine(), out int numberInt);
+
+            if (numberInt > 0)
+            {
+                Console.WriteLine($"Entered number {numberInt} is positive.");
+            }
+            else if (numberInt < 0)
+            {
+                Console.WriteLine($"Entered number {numberInt} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"Entered number {numberInt} equal to 0.");
+            }
+        }
+    }
+}
